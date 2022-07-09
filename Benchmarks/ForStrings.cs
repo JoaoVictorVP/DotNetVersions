5b8b47323145b65568134805b872b6875ca58935
J. V - N <joaovitorsl2012@hotmail.com>
2022-07-09 17:19:03 -0300

    adding some operations with strings in for strings benchmark

--- Benchmarks/ForStrings.cs
+++ Benchmarks/ForStrings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BenchmarkDotNet.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,73 @@ using System.Threading.Tasks;
 namespace DotNetVersions.Benchmarks;
 public class ForStrings
 {
+    private readonly string text;
+    private readonly int textSize;
+
+    public ForStrings()
+    {
+        text = File.ReadAllText("SampleText.txt");
+        textSize = text.Length;
+    }
+
+    [Benchmark]
+    public int FindIndexOf()
+    {
+        return text.IndexOf("Vestibulum ullamcorper odio tellus.");
+    }
+
+    [Benchmark]
+    public string Replace()
+    {
+        return text.Replace("Vestibulum ullamcorper odio tellus.", "This is a test...");
+    }
+
+    [Benchmark]
+    public string Remove()
+    {
+        return text.Remove(textSize - 30);
+    }
+
+    [Benchmark]
+    public bool Contains()
+    {
+        return text.Contains("tellus");
+    }
+
+    [Benchmark]
+    public string[] Split()
+    {
+        return text.Split("tincidunt", StringSplitOptions.RemoveEmptyEntries);
+    }
+
+    [Benchmark]
+    public string Substring()
+    {
+        return text.Substring(textSize - 50, 10);
+    }
+
+    [Benchmark]
+    public ReadOnlySpan<char> SubstringWithSpan()
+    {
+        return text.AsSpan().Slice(textSize - 50, 10);
+    }
+
+    [Benchmark]
+    public long IterateAndSumChars()
+    {
+        long sum = 0;
+        for (int i = 0; i < textSize; i++)
+            sum += text[i];
+        return sum;
+    }
+
+    [Benchmark]
+    public long IterateAndSumCharsUsingSpan()
+    {
+        long sum = 0;
+        var textSpan = text.AsSpan();
+        for (int i = 0; i < textSize; i++)
+            sum += textSpan[i];
+        return sum;
+    }
 }
