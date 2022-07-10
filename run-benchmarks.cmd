7f3d9a0295032d720e552f54606db29fcf477bd6
J. V - N <joaovitorsl2012@hotmail.com>
2022-07-10 00:31:28 -0300

    better run-benchmarks script

--- run-benchmarks.cmd
+++ run-benchmarks.cmd
@@ -1,7 +1,2 @@
-dotnet run -c Release -f net48
-dotnet run -c Release -f netcoreapp3.1
-dotnet run -c Release -f net5.0
-dotnet run -c Release -f net6.0
-mono "bin/Release/net48/DotNetVersions.exe"
-mono "bin/Release/net48/DotNetVersions.exe" --llvm
+dotnet run -c Release -f net7.0
 pause
